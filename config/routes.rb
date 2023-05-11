469a98f80ecc7f5d871f36c828c6dcc7e227e0e4
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-05-11 13:45:51 +0000

    [Fix]public/my_page

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,29 @@ Rails.application.routes.draw do
     get '/items/:id' => 'items#show'
     get '/customers/my_page' => 'customers#show'
     get '/customers/information/edit' => 'customers#edit'
-    
+    patch '/customers' => 'customers#update'
+    get '/customers/confirm' => 'customers#confirm'
+    patch '/customers/withdraw' => 'customers#withdraw'
+    get '/cart_items' => 'cart_items#index'
+    patch '/cart_items/:id' => 'cart_items#update'
+    delete '/cart_items/:id' => 'cart_items#destroy'
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    post '/cart_items' => 'cart_items#create'
+    get '/orders/new' => 'orders#new'
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
+    post '/orders' => 'orders#create'
+    get '/orders' => 'orders#index'
+    get '/orders/:id' => 'orders#show'
   end
 
   namespace :admin do
     root to: "homes#top"
-    get 'items' => 'admin/items#index'
+    resources :items, only: [:index, :new, :create, :show, :edit]
+    patch '/items/:id' => 'items#update'
+    resources :customers, only: [:index, :show, :edit]
+    patch '/customers/:id' => 'customers#update'
+    get '/orders/:id' => 'orders#show'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
