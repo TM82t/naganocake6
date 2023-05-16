dfaf6870c596b693ccc0b0aac272bc16bdf6938a
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-05-16 13:15:31 +0000

    [Add]cart_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  has_many :cart_items
   belongs_to :admin
   has_one_attached :image
 
@@ -8,6 +9,10 @@ class Item < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  
+  def with_tax_price
+    (price * 1.1).floor
   end
 
 end
