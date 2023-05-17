b11d6a009307e9307419267a5a2bb5a7aa5da21e
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-05-17 13:18:59 +0000

    [Fix]cart_item to public/cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,20 +3,22 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items.all
+    @total_payment = 0
   end
 
   def create
-      @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    @cart_item = current_customer.cart_items.new(cart_item_params)
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-        cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-        cart_item.amount += params[:cart_item][:amount].to_i
-        cart_item.save
-        redirect_to cart_items_path
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
     elsif @cart_item.save
-        @cart_items = current_customer.cart_items.all
-        render 'index'
+      @cart_items = current_customer.cart_items.all
+      render 'index'
     else
-        render 'index'
+      render 'index'
     end
   end
 
