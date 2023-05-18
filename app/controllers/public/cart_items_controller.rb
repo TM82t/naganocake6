48e9c3c3c7b5d8d9751d008ce07c75eb578727c6
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-05-18 13:17:53 +0000

    [Fix]cart_items/index to app/views/public

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -22,6 +22,18 @@ class Public::CartItemsController < ApplicationController
     end
   end
 
+  def destroy_all
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.delete
+    redirect_to cart_items_path
+  end
+
   private
 
   def cart_item_params
