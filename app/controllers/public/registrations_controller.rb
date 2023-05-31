e0544e722ab1ae47927a76efe4a6529cf68c30bf
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-05-31 14:33:32 +0000

    [Finish]nagano_cake

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,7 +6,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    items_path
+    customers_my_page_path
   end
 
   def after_sign_out_path_for(resource)
